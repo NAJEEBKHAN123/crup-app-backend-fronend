1f083b36f06f7e17fbd19ecca73ff55a77b01ed4
NAJEEBKHAN123 <najeebkhanlaku.com>
2024-09-26 14:41:18 +0500

    Add basic structure of express

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express = require('express')
-console.log(express)
+
+const PORT = 3000;
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send("hi this is home page")
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
