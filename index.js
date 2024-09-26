08d0524b0939485a1f0b7ad28eb14315013e9a33
NAJEEBKHAN123 <najeebkhanlaku.com>
2024-09-26 20:32:11 +0500

    install core package

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const core = require('core')
 
 const PORT = 3000;
 const app = express();
 app.use(express.json())
+app.use(core())
 
 const users = [
     {id: 1, name: 'najeeb', email: 'najeebkhan@gmail.com'},
@@ -27,7 +29,37 @@ app.post('/api/user', (req, res) =>{
     res.status(201).json({ message: 'new User created!', data: newuser })
 })
 
+//update users
 
+app.put('/api/user/:id', (req, res) =>{
+    const userId = parseInt(req.params.id);
+    const userIndex = users.findIndex(user => user.id === userId)
+    if(userIndex === -1){
+        res.status(404).json({message: "User not found!"})
+    }
+    users[userIndex] ={
+        ...users[userIndex],
+        ...req.body,
+}
+res.status(201).json({ message: 'new User created!', data: users[userIndex] })
+   
+})
+
+// delete user 
+
+app.delete('/api/user/:id',(req, res) =>{
+    const userId = parseInt(req.params.id);
+    const userIndex = users.findIndex(user => user.id === userId)
+
+    if(userIndex === -1){
+        res.status(404).json({message: "User not found!"})
+    }
+    else{
+        users.splice(userIndex, 1);
+        res.status(201).json({ message: 'users deleted successfully!'})
+        
+    }
+})
 app.listen(PORT, () =>{
     console.log(`Server is running on http://localhost:${PORT}`)
 })
