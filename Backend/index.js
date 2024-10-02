25a803d55fbce60b2037c9e98c1dd207780f8c87
NAJEEBKHAN123 <najeebkhanlaku.com>
2024-10-03 00:49:55 +0500

    add

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const PORT = process.env.PORT || 3000;
 
-const PORT = 3000;
 const app = express();
 app.use(express.json())
 app.use(cors())
